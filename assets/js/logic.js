3a8d973af9182900ac7705e2240418870e5c66be
lav3ll <lavell.francis@hotmail.com>
2023-12-01 18:38:03 +0000

    Added js folder and pseudo code

---
+++ assets/js/logic.js
@@ -0,0 +1,12 @@
+// Start button
+
+// Timer presented with each question
+
+// When answered => show next question
+
+// If answered incorrectly => Remove time from the clock
+
+// When all questions are answered the timer is set to zero
+// Game over function
+
+// When game is over => save initials and score to localStorage;
