9aec016b2a0a669a3838d8b4a69182d0e8d5d6bd
lav3ll <lavell.francis@hotmail.com>
2023-12-05 22:22:16 +0000

    Added basic timer

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -12,14 +12,16 @@
 // When game is over => save initials and score to localStorage;
 
 // Bring in variables that will be used
-const timeEl = document.querySelector(".time");
+const timeEl = document.getElementById("time");
 const startBtn = document.getElementById("start");
 const startScreenEl = document.getElementById("start-screen");
 const questionsEl = document.getElementById("questions");
 const questionTitleEl = document.getElementById("question-title");
+const choicesEl = document.getElementById("choices");
 const endScreenEl = document.getElementById("end-screen");
 const feedbackEl = document.getElementById("feedback");
 
+// Create object holding questions
 const questions = [
   {
     title: "Commonly used data types do not include",
@@ -60,12 +62,54 @@ const questions = [
   },
 ];
 
-console.log(questions[0].title);
+// Create Timer
+let timeLeft = 75;
+
+function countdown() {
+  timeEl.textContent = timeLeft;
+
+  const countdownTimer = setInterval(function () {
+    timeLeft--;
+    timeEl.textContent = timeLeft;
+
+    if (timeLeft <= 0) {
+      clearInterval(countdownTimer);
+    }
+  }, 1000);
+}
+
+console.log(questions[1].a[0]);
 
 function startQuiz() {
   alert("Quiz has begun");
   startScreenEl.classList.add("hide");
   questionsEl.classList.remove("hide");
+  generateQuestions();
+  countdown();
 }
 
+// Create a function that generates the questions
+function generateQuestions() {
+  questionTitleEl.textContent = questions[0].title;
+  const oLEl = document.createElement("ol");
+  choicesEl.appendChild(oLEl);
+
+  const choices = ["a", "b", "c", "d"];
+
+  for (let i = 0; i < choices.length; i++) {
+    const liButton = document.createElement("button");
+    const liEl = document.createElement("li");
+
+    liButton.textContent = questions[0][choices[i]][0];
+    liButton.classList.add("choice-button"); // Adding a class for styling (optional)
+
+    const listItemText = document.createTextNode(
+      `${choices[i].toUpperCase()}: `
+    ); // Displaying the list item number
+    liEl.appendChild(listItemText);
+    liEl.appendChild(liButton);
+
+    oLEl.appendChild(liEl);
+  }
+}
 startBtn.addEventListener("click", startQuiz);
